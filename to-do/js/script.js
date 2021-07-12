893d9194fce5889b81841a149e925475c14464c2
frsdjefaflia <djefaflia.fares@gmail.com>
2021-07-12 16:56:21 +0100

    fix all

--- to-do/js/script.js
+++ to-do/js/script.js
@@ -2,10 +2,14 @@ var btnNavbar = document.querySelector(".icon-resp");
 var showIcon = document.querySelector(".fa-bars");
 var hideIcon = document.querySelector(".fa-angle-double-left");
 var list = document.querySelector(".sidebar");
+var hideUl = document.querySelectorAll(".all-ul ul li");
 
 function openList(){
   showIcon.classList.toggle("hide-icon");
   hideIcon.classList.toggle("hide-icon");
   list.classList.toggle("show-aside");
+  for( i=0; i < hideUl.length; i++){
+    hideUl[i].classList.toggle("li-hide");
+  }
 };
 btnNavbar.onclick = openList;
