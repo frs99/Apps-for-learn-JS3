b154ffdd991509bccb9c3bb7ae47dbf867592640
frsdjefaflia <djefaflia.fares@gmail.com>
2021-07-21 15:52:38 +0100

    add and delete todo

--- to-do/js/script.js
+++ to-do/js/script.js
@@ -61,70 +61,24 @@ addTodo.onclick = function(){
 
     var icon = document.createElement("i");
     newLi.appendChild(icon);
-    icon.classList.add("fas", "fa-ellipsis-h", "edit-todo");
+    icon.classList.add("fas", "fa-trash", "btn-delete");
 
     boxData.classList.toggle("show-box-data");
     main[0].classList.toggle("filter");
 
-    //CREATE BOX OP
-
-    var crboxOp = document.createElement("div");
-    newLi.appendChild(crboxOp);
-    crboxOp.classList.add("op-task");
-    var crUl = document.createElement("ul");
-    crboxOp.appendChild(crUl);
-    
-
-    
-    //CREAT BTN RENAME
-    var rnm = document.createElement("li");
-    crUl.appendChild(rnm);
-    rnm.classList.add("li-op");
-    var iconrnm = document.createElement("i");
-    rnm.appendChild(iconrnm);
-    iconrnm.classList.add("fas", "fa-edit")
-    var txtrnm = document.createElement("p");
-    txtrnm.textContent = "Rename"
-    rnm.appendChild(txtrnm);
-
-    //CREATE BTN DELETE
-    var dlt = document.createElement("li");
-    crUl.appendChild(dlt);
-    dlt.classList.add("li-op");
-    var icondlt = document.createElement("i");
-    dlt.appendChild(icondlt);
-    icondlt.classList.add("fas", "fa-trash-alt")
-    var txtdlt = document.createElement("p");
-    txtdlt.textContent = "Delete"
-    dlt.appendChild(txtdlt);
-
-    var toTodo = document.createElement("li");
-    toTodo.classList.add("to-todo");
-    toTodo.textContent = "To Do";
-    crUl.appendChild(toTodo);
-
-    var toDoing = document.createElement("li");
-    toDoing.classList.add("to-doing");
-    toDoing.textContent = "Doing";
-    crUl.appendChild(toDoing);
-
-    var toDone = document.createElement("li");
-    toDone.classList.add("to-done");
-    toDone.textContent = "Done"
-    crUl.appendChild(toDone);
-  }
-}
+    // FOR DELETE TASK TODO
+    var btnDeleteTodo = document.getElementsByClassName("btn-delete");
+    for (iBtnTodo = 0; iBtnTodo < btnDeleteTodo.length; iBtnTodo++){
+      btnDeleteTodo[iBtnTodo].onclick = function(){
+        this.parentNode.remove();
+      };
+    };
+  };
+};
 
 
-//FOR OPEN OP
-var op = listTodo.children;
-var openOp = document.querySelectorAll(".op-task");
-for (iTodo = 0; iTodo < op.length; iTodo++){
-  op[iTodo].onclick = function(){
-    openOp.classList.toggle("show-op-task")
-    openOp.classList.toggle("op-todo")
-  }
-}
+
+
 
 
 // FOR ADD TASK - DOING
@@ -146,3 +100,56 @@ closeData.onclick = function(){
   boxData.classList.toggle("show-box-data");
   main[0].classList.toggle("filter");
 }
+
+
+
+
+
+
+
+//CREATE BOX OP
+
+    // var crboxOp = document.createElement("div");
+    // newLi.appendChild(crboxOp);
+    // crboxOp.classList.add("op-task");
+    // var crUl = document.createElement("ul");
+    // crboxOp.appendChild(crUl);
+    
+
+    
+    // //CREAT BTN RENAME
+    // var rnm = document.createElement("li");
+    // crUl.appendChild(rnm);
+    // rnm.classList.add("li-op");
+    // var iconrnm = document.createElement("i");
+    // rnm.appendChild(iconrnm);
+    // iconrnm.classList.add("fas", "fa-edit")
+    // var txtrnm = document.createElement("p");
+    // txtrnm.textContent = "Rename"
+    // rnm.appendChild(txtrnm);
+
+    // //CREATE BTN DELETE
+    // var dlt = document.createElement("li");
+    // crUl.appendChild(dlt);
+    // dlt.classList.add("li-op");
+    // var icondlt = document.createElement("i");
+    // dlt.appendChild(icondlt);
+    // icondlt.classList.add("fas", "fa-trash-alt")
+    // var txtdlt = document.createElement("p");
+    // txtdlt.textContent = "Delete"
+    // dlt.appendChild(txtdlt);
+
+    // var toTodo = document.createElement("li");
+    // toTodo.classList.add("to-todo");
+    // toTodo.textContent = "To Do";
+    // crUl.appendChild(toTodo);
+
+    // var toDoing = document.createElement("li");
+    // toDoing.classList.add("to-doing");
+    // toDoing.textContent = "Doing";
+    // crUl.appendChild(toDoing);
+
+    // var toDone = document.createElement("li");
+    // toDone.classList.add("to-done");
+    // toDone.textContent = "Done"
+    // crUl.appendChild(toDone);
